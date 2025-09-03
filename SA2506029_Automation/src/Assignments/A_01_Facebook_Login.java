6d6727fe6654531e324c86d1bd32b906407f0728
nitin <nitingupta3005@gmail.com>
2025-09-03 23:31:47 +0530

    03 September

--- SA2506029_Automation/src/Assignments/A_01_Facebook_Login.java
+++ SA2506029_Automation/src/Assignments/A_01_Facebook_Login.java
@@ -5,15 +5,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class A_01_Facebook_Login {
 
-	
 	public static void main(String[] args) throws InterruptedException {
-		
+
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.facebook.com/");
-		
+		 driver.get("https://www.facebook.com/");
+
+
 //		
 //		WebElement username = driver.findElement(By.className("inputtext"));
 //		username.sendKeys("asdfghjkjhgfdsa@gmail.com");
@@ -21,18 +22,18 @@ public class A_01_Facebook_Login {
 //		
 //		WebElement password = driver.findElement(By.className("_9npi"));
 //		password.sendKeys("agjhgfdsdfghjkjhgfds");
-		
-		
+
 		WebElement username=driver.findElement(By.cssSelector("input[id='email']"));
 		username.sendKeys("13243546@gmail.com");
 		WebElement password=driver.findElement(By.cssSelector("input[class='_9npi']"));
 		password.sendKeys("1353463572683");
 		WebElement login=driver.findElement(By.cssSelector("button[class='_42ft']"));
+
 		
 		Thread.sleep(2000);
-		
+
 		driver.close();
-		
+
 	}
-	
+
 }
---
+++ SA2506029_Automation/src/WebdriverDemos/SA_17_checkboxScenario.java
@@ -0,0 +1,46 @@
+package WebdriverDemos;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class SA_17_checkboxScenario extends baseFunction {
+
+	public static void main(String[] args) {
+
+		launchBrowser("chrome");
+		launchURL("https://register.rediff.com/register/register.php?FormName=user_details");
+		
+		
+		WebElement checkBox =driver.findElement(By.cssSelector("input[name^='chk_altemail']"));
+		
+		System.out.println("Before click on Checkbox");
+		
+		System.out.println("checking visibility of checkbox "+ checkBox.isDisplayed());
+		System.out.println("checking default State of checkbox "+ checkBox.isSelected()); // false
+		System.out.println("checking enability of checkbox "+ checkBox.isEnabled());
+		
+		
+		if(!checkBox.isSelected()) {
+			checkBox.click();
+		}
+		
+		System.out.println("-------------------After click on Checkbox-----------------------");
+		
+		System.out.println("checking visibility of checkbox "+ checkBox.isDisplayed());
+		System.out.println("checking default State of checkbox "+ checkBox.isSelected()); // true
+		System.out.println("checking enability of checkbox "+ checkBox.isEnabled());
+		
+		
+		
+		checkBox.click(); // de-select the checkbox
+		
+		System.out.println("-------------------After click on Checkbox-----------------------");
+		
+		System.out.println("checking default State of checkbox "+ checkBox.isSelected()); // false
+		
+		sleep();
+		
+		driver.close();
+	}
+
+}
