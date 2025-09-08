7e348ba6f9a884a98620deaeece9b9e2b0f12c85
nitin <nitingupta3005@gmail.com>
2025-09-08 23:21:03 +0530

    8 September

--- SA2506029_Automation/src/WebdriverDemos/baseFunction.java
+++ SA2506029_Automation/src/WebdriverDemos/baseFunction.java
@@ -30,7 +30,7 @@ public class baseFunction {
 	public static void sleep() {
 
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(5000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
