be40fa2b4ebc0ad7f37d511e49c0982864ac1890
nitin <nitingupta3005@gmail.com>
2025-10-13 22:04:54 +0530

    remove gittoken

--- SA2506029_RestAssured/src/test/java/AuthenticationType/BearerTokenAuthentication.java
+++ SA2506029_RestAssured/src/test/java/AuthenticationType/BearerTokenAuthentication.java
@@ -9,7 +9,7 @@ import io.restassured.response.Response;
 public class BearerTokenAuthentication {
 
 	
-	String gitToken = "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";
+	String gitToken = "use user specific token";
 	
 	
 	@Test
