ec841f2adea58dc1585a9d3bbb9e15587a2dd565
nitin <nitingupta3005@gmail.com>
2025-10-28 11:13:59 +0530

    28 OCt

--- AppiumTest/src/test/java/MobileEvents/clickGesture.java
+++ AppiumTest/src/test/java/MobileEvents/clickGesture.java
@@ -9,18 +9,25 @@ import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
 import projectUtils.AppiumDriverinitialization;
+import projectUtils.coreFunctions;
 
 public class clickGesture {
 
 	@Test
 	public void testClickGestures() {
 		AppiumDriver driver = AppiumDriverinitialization.createAppiumDriver();
+		
+		
+		coreFunctions.takeScreenShot(driver, "Home_Page");
+		
 		WebElement ele1 = driver.findElement(AppiumBy.accessibilityId("Access'ibility"));
 		
 		// Java
 		driver.executeScript("mobile: clickGesture", ImmutableMap.of(
 		    "elementId", ((RemoteWebElement) ele1).getId()
 		));
+		
+		coreFunctions.takeScreenShot(driver, "Accessbility_Page");
 
 	}
 
